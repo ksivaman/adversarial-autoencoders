9c67f55241d8447bdc86ec7374617c1e8b6e9978
ksivaman <ksivaman@purdue.edu>
2019-09-15 23:56:47 -0400

    feature extractor to CUDA

--- mnist_scripts/feature_losses.py
+++ mnist_scripts/feature_losses.py
@@ -4,6 +4,7 @@ import torch
 import torchvision
 import torch.nn.functional as F
 import torch.optim as optim
+
 from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -13,7 +14,11 @@ from torchvision.utils import save_image
 from torchvision.datasets import MNIST
 from torchvision import datasets
 
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+
 my_vgg = vgg16(pretrained=True)
+my_vgg.to(device)
 
 def get_features_L3(input_image):
     return my_vgg.features[:-3](input_image)
@@ -21,14 +26,12 @@ def get_features_L3(input_image):
 model = Digits()
 model.load_state_dict(torch.load('models/mnist_cnn.pt'))
 
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda:0" if use_cuda else "cpu")
-
 kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data', train=True,
                     transform=transforms.Compose([
+                        transforms.Grayscale(num_output_channels=3),
                         transforms.ToTensor(),
                         transforms.Normalize((0.1307,), (0.3081,))
                     ])),
@@ -36,7 +39,7 @@ train_loader = torch.utils.data.DataLoader(
 
 test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('./data', train=False, transform=transforms.Compose([
-                        # transforms.Grayscale(num_output_channels=3),
+                        transforms.Grayscale(num_output_channels=3),
                         transforms.ToTensor(),
                         transforms.Normalize((0.1307,), (0.3081,))
                     ])),
@@ -50,19 +53,5 @@ correct = 0
 with torch.no_grad():
     for data, target in test_loader:
         data, target = data.to(device), target.to(device)
-        output = model(data)
-        test_loss += F.nll_loss(output, target, reduction='sum').item()
-        pred = output.argmax(dim=1, keepdim=True)
-        correct += pred.eq(target.view_as(pred)).sum().item()
-
-test_accuracy = correct / (10 * len(test_loader))
-
-test_loss /= len(test_loader.dataset)
-print('Test accuracy: {}'.format(test_accuracy))
-
-with torch.no_grad():
-    for data, target in test_loader:
-        data, target = data.to(device), target.to(device)
         output = get_features_L3(data)
-        print(output.shape)
 
